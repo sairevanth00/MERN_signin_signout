49f2fddc699f3f630591bfb96acc7c947851b4b1
Sai Revanth <86770889+sairevanth00@users.noreply.github.com>
2025-07-17 10:27:13 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ dotenv.config();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://mern-signin-up-demo.netlify.app'
+  'https://mern-signin-up-demo.netlify.app/'
 ];
 
 app.use(cors({
